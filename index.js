9513fd84f323759984f193d509c217931f6b6c7e
lis-dima <lisiev.dmytro@gmail.com>
2022-11-22 17:31:53 +0000

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+const {
+  Connection,
+  PublicKey,
+  clusterApiUrl,
+  Keypair,
+  LAMPORTS_PER_SOL,
+} = require("@solana/web3.js");
+
+const wallet = new Keypair();
+const pubKey = new PublicKey(wallet._keypair.publicKey);
+const secretKey = wallet._keypair.secretKey;
+
+console.log(pubKey);
